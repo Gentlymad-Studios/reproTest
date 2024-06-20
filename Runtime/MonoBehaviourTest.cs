092b755c608c8fe67781d22326e8822fb3b62274
Matthias Guntrum <m.guntrum@gentlymadstudios.com>
2024-06-20 16:57:13 +0200

    initial commit

---
+++ Runtime/MonoBehaviourTest.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+public class MonoBehaviourTest : MonoBehaviour {
+    public BaseData data;
+}
---
+++ Runtime/ScriptableObjectTest.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CreateScriptable",menuName = "Test", order = 0)]
+public class ScriptableObjectTest : ScriptableObject {
+    public BaseData baseData;
+    public GameObject goRef;
+}
